bf4083c658557a84aeef1f840973a1c9ae4b220c
wkrdixon <wkrdixon@670ea90a-9a5c-11de-993b-6301ed8e1f60>
2009-09-15 21:06:45 +0000

    merged ingrids logger changes with mine

--- theTravelExperts/src/com/travelexperts/TXLogger.java
+++ theTravelExperts/src/com/travelexperts/TXLogger.java
@@ -1,6 +1,14 @@
 package com.travelexperts;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.xml.stream.FactoryConfigurationError;
 
 import org.apache.log4j.Appender;
 import org.apache.log4j.ConsoleAppender;
@@ -12,13 +20,15 @@ import org.apache.log4j.PatternLayout;
 
 /**
  * 
- * @author Will_Dixon
+ * @author Will Dixon & Ingrid Niu
  * 
  * 
  *         Logger that saves errors/warnings in XML
  */
 public class TXLogger extends Logger
 {
+	
+	// Ingrids log4J stuff
 	protected static final String LOG_NAME = "TXLog";
 	static Logger logger = Logger.getLogger(LOG_NAME);
 
@@ -43,9 +53,66 @@ public class TXLogger extends Logger
 		}
 		catch (IOException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return logger;
 	}
+
+	// Wills stuff
+	
+	// A few different error levels
+	public static final int EVENT_ERROR = 1;
+	public static final int EVENT_WARNING = 2;
+	public static final int EVENT_NOTICE = 3;
+	public static final int EVENT_INFO = 4;
+	
+	private static final String[] EVENT_NAMES = { "Unknown", "Error", "Warning", "Notice", "Info" };
+
+	private static String LOG_FILENAME = "txlog.txt";
+	
+	private static BufferedWriter bfwLogger;
+	
+	// Example of static constructor
+ 	static {
+				
+		try {			
+			bfwLogger = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(LOG_FILENAME, true)));
+			logEvent(EVENT_INFO, "Log started");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (FactoryConfigurationError e) {
+			e.printStackTrace();
+		}
+ 	}
+ 	
+ 	private static void logEvent(int eventLevel, String eventMessage) {
+ 		try {
+ 			if(eventLevel >= EVENT_NAMES.length) {
+ 				bfwLogger.write("Internal logger error: invalid error code");
+ 				return;
+ 			}
+ 			String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+ 			bfwLogger.write(EVENT_NAMES[eventLevel] + " @ " + timeStamp + ": " + eventMessage);
+ 			bfwLogger.write("\r\n");
+	 		bfwLogger.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+ 	}
+
+ 	
+ 	public static void logError(String errorMessage) {
+ 		logEvent(EVENT_ERROR, errorMessage);
+ 	}
+ 	public static void logWarning(String errorMessage) {
+ 		logEvent(EVENT_WARNING, errorMessage);
+ 	}
+ 	public static void logNotice(String errorMessage) {
+ 		logEvent(EVENT_NOTICE, errorMessage);
+ 	}
+ 	public static void logInfo(String errorMessage) {
+ 		logEvent(EVENT_INFO, errorMessage);
+ 	}
+ 	
+	
 }
